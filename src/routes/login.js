95fb3c9ed3491eb95771ea8465b42da88a03c282
Koen Raijer <koenraijer@gmail.com>
2022-04-23 13:07:29 +0200

    access_token / refresh token obtained

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,46 @@
+const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
+const redirect_uri = "http://localhost:3000/login"
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+let acc_token;
+let expires_at;
+let time_left;
+
 export async function get({ url }) {
     let params = url.href.toString().split('?')[1]; // split creates an array of 2 strings, and we need the string at index [1]
     const code = new URLSearchParams(params).get('code')
 
-    return {
-      body: {
-        code: code
+    // Check if there's a token already, and whether it is still valid
+    if(acc_token && expires_at > Date.now()) {
+      return {
+          status: 200,
+          body: {acc_token, time_left}
       }
     }
+
+    const { access_token, expires_in, refresh_token } = await fetch(TOKEN_ENDPOINT, {
+        method: 'POST',
+        headers: {
+        // Authorization: `Basic ${basic}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+          grant_type: 'authorization_code',
+          code,
+          redirect_uri,
+          client_id,
+          client_secret,
+        })
+    }).then(res => res.json());
+
+      acc_token = access_token
+      time_left = expires_in
+      expires_at = Date.now() + (expires_in * 1000) // expires_in is in seconds, while Date.now() is in milliseconds
+
+      return {
+          status: 200,
+          body: {access_token: acc_token, expires_in: expires_in, refresh_token: refresh_token }
+      }   
 }
+
+   
